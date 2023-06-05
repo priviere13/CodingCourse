96b5b0f31a9f68f7af775cc9120a6469808f7c23
priviere13 <pauline.riviere@gmx.com>
2023-06-05 15:42:18 +0100

    Exercises for Lab 3

---
+++ week3/test.py
@@ -0,0 +1,106 @@
+#print(1==1)
+
+#if 4==1 and 3>3:
+ #   print("hello")
+#else:
+#    print ("fff")
+
+#input_name = input("What's your name?")
+#print(f"Hi {input_name}")
+
+#user_height = int(input("Your height?"))
+#if int(user_height) > 160:
+#    print("All rides")
+#elif int(user_height) > 150:
+#   print("All rides but big one")
+#else:
+#    print ("Too short")
+
+
+#print(random.randint(0,40))
+
+#temp = random.randint(0,40)
+
+
+###### LAB EXERCISES 
+
+#1
+
+# 3+2 == 5, True
+# "Orange" == "Orange" OR 3 < 2, True
+# "Orange" != "Apple", True
+# “Orange” != “Apple” AND 3 < 2, False
+# NOT(“Orange” != “Apple” AND 3 < 2), True
+# 25 >= 67 AND 18 <= 53, False
+
+#2
+
+#user_age = int(input("What's your age?"))
+#if int(user_age) > 18:
+#    print("You can watch the James Bond film")
+#else:
+#    print("You are too young to watch the James Bond film")
+
+#3
+
+#test_score = int(input("What's your test score?"))
+#if int(test_score) >= 80:
+#    print("Your grade is A")
+#elif int(test_score) >= 70:
+#    print ("Your grade is B")
+#elif int(test_score) >= 60:
+#   print ("Your grade is C")
+#elif int(test_score) >= 50:
+#    print ("Your grade is D")
+#else:
+#    print ("Your grade is F")
+
+#4
+
+
+#people = ["Isla", "Oliver", "Muhammad", "Freya", "Mia"]
+
+#for element in people:
+#    print(f"Hello {element}")
+
+#5
+
+import random
+
+wanna_play = input("Do you want to play? ")
+while wanna_play == "yes":
+
+    random.randint(1, 2)
+    if random.randint(1, 2) == 1:
+      print("Heads")
+    else:
+       print ("Tails")
+    wanna_play = input("Do you want to play? ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
